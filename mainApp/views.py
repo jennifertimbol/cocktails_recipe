e2b507cc30642fb658984242b75428c6aeb1a0bd
Dawa Sherpa <dawaad0123@gmail.com>
2021-05-21 19:02:56 -0700

    added to profilepage

--- mainApp/views.py
+++ mainApp/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from .models import *
 import bcrypt
 from .forms import recipeForm
+import requests
 
 def homepage(request):
     context = {
@@ -50,8 +51,12 @@ def userprofile(request):
     if 'curr_user' not in request.session:
         return redirect('/')
     user = User.objects.get(id=request.session['curr_user'])
+    response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
     context= {
         'user': user
+        'drink_name' = response.Drink.json()
+        'drink_image' = response.DrinkThumb.json()
+        'drink_instruction' = response.Instructions.json()
     }
     return render(request, "profilepage.html", context)
 
@@ -82,4 +87,13 @@ def uploadrecipe(request):
 def logout(request):
     request.session.flush()
     return redirect('/')
+
+def generate_api_recipe(request):
+    response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+    context = {
+        'drink_name' = response.Drink.json()
+        'drink_image' = response.DrinkThumb.json()
+        'drink_instruction' = response.Instructions.json()
+    }
+    return render(request, 'homepage.html', context)
 
